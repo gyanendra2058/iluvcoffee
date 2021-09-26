727ad7d3b1b37023339ee3f8b6698256d35365bb
Gyanendra Chaubey <gyanendra2058@users.noreply.github.com>
2021-09-26 12:39:25 +0530

    Initial commit

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+    @Get()
+    findAll(): string {
+        return 'This action returns all coffees';
+    }
+}
